09575d1c8b4d25c8f74fab1a0542cb1d9c263657
Jade True <jadetruexo@gmail.com>
2022-04-12 12:21:26 +0100

    Changed highlight text colour

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 const Layout: React.FC = ({children}) => {
     return (
         <>
-            <div className="min-h-screen p-5 md:min-h-full">
+            <div className="min-h-screen p-5 selection:bg-cyan-500 selection:text-white md:min-h-full">
                 {/* <Naivgation /> */}
                 <main className="m-auto flex max-w-6xl flex-col justify-center py-8 text-center">
                     {children}
