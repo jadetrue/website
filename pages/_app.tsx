3f27970828ad133b6d2937660d2f2924e8c5f4fd
Jade True <jadetruexo@gmail.com>
2022-04-13 10:27:11 +0100

    Updated Google Analytics to be correct

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,7 @@
 import "../styles/fontawesome";
 import "../styles/global.css";
 import {AppProps} from "next/app";
-import Script from "next/script";
 
 export default function App({Component, pageProps}: AppProps) {
-    return (
-        <>
-            <Script
-                strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            />
-            <Script strategy="lazyOnload">
-                {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-            });
-        `}
-            </Script>
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
