028e5b2c088de1471c727b4979208b5097138fee
Jade True <jadetruexo@gmail.com>
2022-04-13 16:32:43 +0100

    Made reusable component for social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Button from "./Button";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import SocialIcons from "./SocialIcons";
 
 const Footer = () => {
     return (
@@ -11,44 +10,7 @@ const Footer = () => {
                         Say hi!
                     </h3>
                 </div>
-                <div className="flex w-full flex-row items-center justify-center gap-4 py-4 text-xl md:gap-8">
-                    <Button
-                        type="link"
-                        href="https://github.com/jadetruexo"
-                        styles="hover:animate-bounce"
-                    >
-                        <FontAwesomeIcon
-                            icon={{prefix: "fab", iconName: "github"}}
-                        />
-                    </Button>
-                    <Button
-                        type="link"
-                        href="https://www.linkedin.com/in/jadetrue/"
-                        styles="hover:animate-bounce"
-                    >
-                        <FontAwesomeIcon
-                            icon={{prefix: "fab", iconName: "linkedin"}}
-                        />
-                    </Button>
-                    <Button
-                        type="link"
-                        href="https://twitter.com/jadetruexo"
-                        styles="hover:animate-bounce"
-                    >
-                        <FontAwesomeIcon
-                            icon={{prefix: "fab", iconName: "twitter"}}
-                        />
-                    </Button>
-                    <Button
-                        type="link"
-                        href="mailto:jadetruexo@gmail.com"
-                        styles=" hover:animate-bounce"
-                    >
-                        <FontAwesomeIcon
-                            icon={{prefix: "fas", iconName: "at"}}
-                        />
-                    </Button>
-                </div>
+                <SocialIcons type="footer" />
             </div>
             <div className="flex justify-center pb-8">
                 <p className="text-md">
---
+++ components/SocialIcons.tsx
@@ -0,0 +1,91 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React from "react";
+import Button from "./Button";
+
+export interface Props {
+    type: "header" | "footer";
+}
+
+const SocialIcons: React.FC<Props> = ({type}) => {
+    if (type === "header")
+        return (
+            <div className="mt-20 flex flex-row flex-wrap items-center gap-8">
+                <div className="hidden h-1 w-40 items-center rounded-full bg-black align-middle md:flex" />
+                <Button
+                    type="link"
+                    href="https://github.com/jadetruexo"
+                    styles="hover:animate-bounce"
+                >
+                    <FontAwesomeIcon
+                        icon={{prefix: "fab", iconName: "github"}}
+                        className="text-5xl hover:animate-bounce"
+                    />
+                </Button>
+                <Button
+                    type="link"
+                    href="https://www.linkedin.com/in/jadetrue/"
+                    styles="hover:animate-bounce"
+                >
+                    <FontAwesomeIcon
+                        icon={{prefix: "fab", iconName: "linkedin"}}
+                        className="text-5xl hover:animate-bounce"
+                    />
+                </Button>
+                <Button
+                    type="link"
+                    href="https://twitter.com/jadetruexo"
+                    styles="hover:animate-bounce"
+                >
+                    <FontAwesomeIcon
+                        icon={{prefix: "fab", iconName: "twitter"}}
+                        className="text-5xl hover:animate-bounce"
+                    />
+                </Button>
+                <Button
+                    type="link"
+                    href="mailto:jadetruexo@gmail.com"
+                    styles=" hover:animate-bounce"
+                >
+                    <FontAwesomeIcon
+                        icon={{prefix: "fas", iconName: "at"}}
+                        className="text-5xl hover:animate-bounce"
+                    />
+                </Button>
+            </div>
+        );
+
+    return (
+        <div className="flex w-full flex-row items-center justify-center gap-4 py-4 text-xl md:gap-8">
+            <Button
+                type="link"
+                href="https://github.com/jadetruexo"
+                styles="hover:animate-bounce"
+            >
+                <FontAwesomeIcon icon={{prefix: "fab", iconName: "github"}} />
+            </Button>
+            <Button
+                type="link"
+                href="https://www.linkedin.com/in/jadetrue/"
+                styles="hover:animate-bounce"
+            >
+                <FontAwesomeIcon icon={{prefix: "fab", iconName: "linkedin"}} />
+            </Button>
+            <Button
+                type="link"
+                href="https://twitter.com/jadetruexo"
+                styles="hover:animate-bounce"
+            >
+                <FontAwesomeIcon icon={{prefix: "fab", iconName: "twitter"}} />
+            </Button>
+            <Button
+                type="link"
+                href="mailto:jadetruexo@gmail.com"
+                styles=" hover:animate-bounce"
+            >
+                <FontAwesomeIcon icon={{prefix: "fas", iconName: "at"}} />
+            </Button>
+        </div>
+    );
+};
+
+export default SocialIcons;
